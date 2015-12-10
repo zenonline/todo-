6b63eb89b106520a0c7a689ea08c4ed0241b4757
Zen Ives <dev@zenonline.co.uk>
2015-12-10 14:00:35 +0000

    add post for todo

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 var express = require('express');
 var app = express();
+var body_parser = require('body-parser');
+
+////////////////////////////////////////////////////////////////////////
+
 var PORT = process.env.PORT || 3000
-var todos =[{
-	id:1,
-	description: "Eat some food",
-	completed: false
-},{
-	id:2,
-	description: "have a bath",
-	completed: false
-},{
-	id:3,
-	description: "sleep",
-	completed: true
-}]
+var todos =[];
+var todo_next_id =1;
+
+app.use(body_parser.json())
+
+////////////////////////////////////////////////////////////////////////
 
 app.get('/', function (req, res){
 	res.send('Todo API Root');
 });
 
+///////////////////////////////////////////////////////////////////////////
 
 app.get('/todos', function (req, res){
 	res.json(todos);
 });
+
+/////////////////////////////////////////////////////////////////////////////
 
 app.get('/todos/:id', function (req, res){
 	var todo_id = req.params.id;
@@ -39,6 +39,24 @@ app.get('/todos/:id', function (req, res){
 	}
 	//res.send('Asking for todo with id of '+ req.params.id+ req.params.description);
 })
+
+///////////////////////////////////////////////////////////////////////////////////
+
+app.post('/todos', function (req, res){
+	var body = req.body;
+	body.id = todo_next_id++;
+	todos.push(body);
+	/*todos.push({
+		"id": todo_next_id++,
+		"description":body.description,
+		"completed": body.completed
+	});*/
+	res.json(body);
+	console.log("the todos array:"+todos);
+});
+
+////////////////////////////////////////////////////////////////////////////////////////
+
 app.listen(PORT, function (){
 	console.log('Express listening on port '+ PORT);
 });
